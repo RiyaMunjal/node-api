e8b4eba12c8e04d5d3a57e2afdb2a3203b328d81
RiyaMunjal <riyamunjal245@gmail.com>
2024-01-04 14:42:05 +0530

    fixed server

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ ConnectDB();
 
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`server is lstening on the Port: ${process.env.PORT}`)
+    console.log(`server is lstening on the Port: ${process.env.PORT} on ${process.env.NODE_ENV} mode`)
 })
